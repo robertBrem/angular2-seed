129e57b0538d3c8e45be5ad57b5793fc52a8aeaa
angular-cli <angular-cli@angular.io>
2016-06-12 11:36:08 +0200

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ src/app/devoxx-adesso.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'devoxx-adesso-app',
+  templateUrl: 'devoxx-adesso.component.html',
+  styleUrls: ['devoxx-adesso.component.css']
+})
+export class DevoxxAdessoAppComponent {
+  title = 'devoxx-adesso works!';
+}
